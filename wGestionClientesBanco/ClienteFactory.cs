01f4fef47852b3713ace3c7ac3605ea0313b73e5
ejarama <elijllo10@gmail.com>
2025-03-24 13:54:37 -0500

    Se implementa patrón Factory en clase ClienteFactory

---
+++ wGestionClientesBanco/ClienteFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wGestionClientesBanco
+{
+    class ClienteFactory
+    {
+        public static Cliente CrearCliente(string tipo, string nombre, string identificacion, decimal saldo, int cantidadCuentasActivas = 0)
+        {
+            if (tipo == "Corporativo")
+            {
+                return new ClienteCorporativo(nombre, identificacion, saldo);
+            }
+            else if (tipo == "Individual")
+            {
+                return new ClienteIndividual(nombre, identificacion, saldo, cantidadCuentasActivas);
+            }
+            else
+            {
+                throw new ArgumentException("Tipo de empleado no valido");
+            }
+
+        }
+    }
+}
