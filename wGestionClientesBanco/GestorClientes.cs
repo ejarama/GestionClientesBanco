3f05ea30ef41c413585292404e29a90015489a56
ejarama <elijllo10@gmail.com>
2025-03-24 15:32:23 -0500

    se implementa clase GestorClientes

---
+++ wGestionClientesBanco/GestorClientes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wGestionClientesBanco
+{
+    class GestorClientes
+    {
+        private static GestorClientes _instancia;
+
+        private List<Cliente> clientes;
+
+        private GestorClientes()
+        {
+            clientes = new List<Cliente>();
+        }
+
+        private static GestorClientes Instancia
+        {
+            get
+            {
+                if (_instancia == null)
+                {
+                    _instancia = new GestorClientes();
+
+                }
+                return _instancia;
+                    
+            }
+        }
+
+        public void AgregarCliente(Cliente cliente)
+        {
+            if (clientes.Any(c => c.Identificacion == cliente.Identificacion))
+                throw new InvalidOperationException("Ya existe un cliente con esta identificación.");
+            clientes.Add(cliente);
+        }
+
+        public void EliminarCliente(string identificacion)
+        {
+            var cliente = clientes.FirstOrDefault(c => c.Identificacion == identificacion);
+            if (cliente != null)
+                clientes.Remove(cliente);
+            else
+                throw new KeyNotFoundException("Cliente no encontrado.");
+        }
+
+        public List<Cliente> ObtenerClientes()
+        {
+            return clientes;
+        } 
+    }
+}
